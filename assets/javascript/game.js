94e67ec5ddf9f76dd28872987f649e3bb8fb4864
Em Coraccio <emcoraccio@gmail.com>
2019-07-19 23:51:31 -0400

    added ability to choose next enemy to fight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,15 +9,12 @@ let characters = {
   leia: {
     health: 150,
     attack: 9
-  }, 
+  },
 
   yoda: {
     health: 90,
     attack: 15
   },
-
-
-// let imperial = {
 
   atdp: {
     health: 145,
@@ -36,14 +33,9 @@ let characters = {
 
 }
 
-function whichSide (event) {
 
-}
+$(document).ready(function () {
 
-
-
-$(document).ready(function(){
-  
   let screen1 = $('div.screen1');
   let screen2 = $('div.screen2');
   let screen3 = $('div.screen3');
@@ -72,11 +64,12 @@ $(document).ready(function(){
   let enemyHealthText;
   let heroHealth;
   let enemyHealth;
-  
+  let heroSide;
+
   let battleStarted = false;
 
 
-  let reset = function() {
+  let reset = function () {
     lukeHealth.text(characters.luke.health);
     leiaHealth.text(characters.leia.health);
     yodaHealth.text(characters.yoda.health);
@@ -87,10 +80,10 @@ $(document).ready(function(){
 
   reset();
   //choose imperial or rebel
-  $("span.choice").on('click', function(event){
+  $("span.choice").on('click', function (event) {
     let _this = $(this);
     let choice = _this.attr("choice");
-    if(choice === "rebels"){
+    if (choice === "rebels") {
       rebelsDiv.fadeIn();
     }
     else {
@@ -98,13 +91,13 @@ $(document).ready(function(){
     }
     screen2.hide();
   });
-  
+
   //choose a character
-  characterImg.on('click', function(event){
+  characterImg.on('click', function (event) {
     let _this = $(this);
     console.log(chosenHeroDiv.has('img').length);
-    
-    if(!chosenHeroDiv.has('img').length){
+
+    if (!chosenHeroDiv.has('img').length) {
       _this.next().addClass('hero-health')
       heroHealthText = $('p.hero-health > span')
       chosenHeroDiv.append(_this.next());
@@ -112,18 +105,20 @@ $(document).ready(function(){
       (_this).addClass('chosen-hero')
       chosenHero = $('img.chosen-hero');
 
-      
+
       screen3.fadeIn();
-      
-      if(_this.attr("side") === "rebel"){
+
+      if (_this.attr("side") === "rebel") {
         rebelsDiv.fadeOut();
+        heroSide = "rebels";
       }
       else {
         imperialsDiv.fadeOut();
+        heroSide = "imperials";
       }
       chooseCharacText.text("choose a character to fight")
       console.log(chosenHeroDiv.has('img').length);
-    } 
+    }
     else if (!chosenEnemyDiv.has('img').length) {
       _this.next().addClass('enemy-health')
       enemyHealthText = $('p.enemy-health > span')
@@ -138,58 +133,69 @@ $(document).ready(function(){
 
   // hover display health
   characterImg.mouseenter(
-    function() {
+    function () {
       $(this).next().slideDown();
     }
   )
   characterImg.mouseleave(
-    function(){
+    function () {
       $(this).next().slideUp();
     }
   )
 
-  let setChosenValues = function() {
-    if(chosenHeroDiv.has('img').length && chosenEnemyDiv.has('img').length) {
+  let setChosenValues = function () {
+    if (chosenHeroDiv.has('img').length && chosenEnemyDiv.has('img').length) {
       // let chosenHero = $('img.chosen-hero');
       // let chosenEnemy = $('img.chosen-enemy');
-  
+
       let heroName = chosenHero.attr('name');
       let enemyName = chosenEnemy.attr('name');
-  
+
       enemyAttack = characters[enemyName]['attack'];
       heroAttack = characters[heroName]['attack'];
-  
+
       heroHealth = characters[heroName]['health'];
       enemyHealth = characters[enemyName]['health'];
-      
-      return [heroHealth, enemyHealth] 
+
+      return [heroHealth, enemyHealth]
     }
   }
 
-  $('button.attack').on('click', function(){
-    if(!battleStarted) {
+  $('button.attack').on('click', function () {
+    if (!battleStarted) {
       setChosenValues();
       newAttack = parseInt(heroAttack);
     }
 
-    let isDefeated = function() {
-      if (enemyHealth <= 0){
-        chosenEnemyDiv.fadeOut('slow');
-  
-    }
-  
+    let isDefeated = function () {
+      if (enemyHealth <= 0) {
+        chosenEnemy.removeClass('chosen-enemy')
+        chosenEnemyDiv.contents().appendTo('div.defeated-enemies');
+        $('button.attack').hide();
+
+        if (heroSide === "rebels") {
+
+          if (imperialsDiv.has('img').length) {
+            chooseCharacText.text("choose another character to fight").fadeIn();
+          }
+          else {
+            screen3.fadeOut();
+          }
+        }
+      }
+
     }
 
     enemyHealth -= parseInt(newAttack);
     isDefeated();
     heroHealth -= parseInt(enemyAttack)
-  
+
     newAttack += heroAttack;
     heroHealthText.text(heroHealth)
     enemyHealthText.text(enemyHealth)
 
     battleStarted = true;
-    
+
   });
 
 
